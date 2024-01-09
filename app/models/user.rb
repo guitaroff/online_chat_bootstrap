47d997b872e07d6a4c10c83f86d08dee0b8668b6
andrey <ag82pishi@yandex.ru>
2024-01-10 02:15:40 +0300

    adds User, authentication, root page, and adds initial users

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  DEFAULT_PASSWORD = ENV['DEFAULT_PASSWORD'] || 'Example123456_'
+end
