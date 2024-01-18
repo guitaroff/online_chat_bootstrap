fd765a829ac20a6712f2b791d4d6d106d529ab48
andrey <ag82pishi@yandex.ru>
2024-01-18 19:48:57 +0300

    adds private chats

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+ 
+    @room = Room.fetch_room(fetch_room_title(@user, current_user), current_user)
+    @messages = @room.messages
+    @new_message = current_user.messages.build
+  end
+
+  private
+
+  def fetch_room_title(u1, u2)
+    users = [u1,u2].sort
+    "private_#{users[0].id}_#{users[1].id}"
+  end
+end
--- app/models/room.rb
+++ app/models/room.rb
@@ -5,4 +5,8 @@ class Room < ApplicationRecord
   after_create_commit { broadcast_append_to "rooms" }
 
   scope :public_rooms, -> { where(is_private: false) }
+
+  def self.fetch_room(room_title, user)
+    Room.where(title: room_title).first || user.rooms.create!(title: room_title, is_private: true)
+  end
 end
